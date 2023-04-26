cb59752dfca88a1eb6968d14381d6c1880d65662
adeganteng <wicaksonoade21@gmail.com>
2023-04-26 08:32:29 +0700

    update dark mode + perbaikan fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,6 +6,7 @@ hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 });
+
 // Tambahkan kode berikut untuk menghapus navbar ketika layar di klik
 document.addEventListener('click', function (event) {
   if (!navMenu.classList.contains('hidden') && !event.target.closest('#hamburger') && !event.target.closest('#nav-menu')) {
@@ -15,6 +16,7 @@ document.addEventListener('click', function (event) {
 });
 // Tambahkan kode berikut untuk menghapus navbar ketika salah satu <li> di dalamnya diklik
 const navItems = document.querySelectorAll('#nav-menu li');
+
 navItems.forEach(function (navItem) {
   navItem.addEventListener('click', function () {
     hamburger.classList.remove('hamburger-active');
@@ -26,10 +28,36 @@ navItems.forEach(function (navItem) {
 window.onscroll = function () {
   const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top')
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
   } else {
     header.classList.remove('navbar-fixed');
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
   }
 };
+
+//Dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle')
+const html = document.querySelector('html')
+
+darkToggle.addEventListener('click' , function(){
+  if (darkToggle.checked){
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  }else{
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+//pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
